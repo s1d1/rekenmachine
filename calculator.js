5ae05c50f04cfbe539194da47d1c59c9b6847da2
Sarah Danner <sarahdanner@air-van-sarah.home>
2021-04-28 18:17:18 +0200

    display value digits and decimal

--- calculator.js
+++ calculator.js
@@ -12,6 +12,29 @@ const calculator = {
     first: null,
     wait_on_second: false,
     operator: null,
+  };
+
+function inputDigit(digit) {
+    const {displayValue} = calculator;
+    // change displayValue on calculator to updated displayvalue
+    // if displayvalue is 0, replace it with the digit, otherwise append digit to the displayvalue
+    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+}
+
+function inputDecimal(dot) {
+    // if the displayvalue of the calculator does not already contain a decimal, add it 
+    if (!calculator.displayValue.includes(dot)) {
+        calculator.displayValue += dot;
+    }
+}
+
+function handleOperator(nextOperator) {
+    const {first, displayValue, operator} = calculator;
+
+    // make string displayValue a float
+    const inputValue = parseFloat(displayValue);
+
+
 }
 
 function updateDisplay() {
@@ -22,13 +45,13 @@ function updateDisplay() {
     display.value = calculator.displayValue;
 }
 
-// event delegation: 
-const keys = document.querySelector('.calculator-keys');
+updateDisplay();
 
+const keys = document.querySelector('.calculator-keys');
 // set an eventlistener for when type 'click' that will dispatch an event that is explained after the arrow
 keys.addEventListener('click', (event) => {
     // set the clicked element as target
-    const {target} = event;
+    const { target } = event;
 
     // check if target is a button
     // if not, exit
@@ -40,29 +63,28 @@ keys.addEventListener('click', (event) => {
     // print string for check
 
     // classlist.contains checks for string ('operator') in all the classes within the target 
-    if (target.classlist.contains('key-operator')) {
+    if (target.classList.contains('key-operator')) {
         console.log('key-operator', target.value);
         return;
     }
 
-    if (target.classlist.contains('decimal')) {
-        console.log('decimal', target.value);
+    if (target.classList.contains('decimal')) {
+        inputDecimal(target.value);
+        updateDisplay();
         return;
     }
 
-    if (target.classlist.contains('clear')) {
+    if (target.classList.contains('clear')) {
         console.log('clear', target.value);
         return;
     }
 
-    if (target.classlist.contains('key-equal')) {
+    if (target.classList.contains('key-equal')) {
         console.log('key-equal', target.value);
         return;
     }
 
-    if (target.classlist.contains('number')) {
-        console.log('number', target.value);
-        return;
-    }
+    inputDigit(target.value);
+    updateDisplay();
 
 });
