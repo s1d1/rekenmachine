e68b634fd8250d43d74cbf33d1418830bed5f98a
Sarah Danner <sarahdanner@air-van-sarah.home>
2021-04-30 11:39:18 +0200

    used stylesheet from source

--- calculator.js
+++ calculator.js
@@ -3,6 +3,7 @@ formula = [];
 
 buttons.forEach(function(button) {
     button.addEventListener('click', get_formula);
+
 });
 
 function get_formula(event) {
@@ -12,22 +13,18 @@ function get_formula(event) {
         formula.push(button_value);
         document.getElementById("screen").value = update_screen('number', button_value);
     }
-
     else if (event.target.classList.contains('decimal')) {
         formula.push(button_value);
         document.getElementById("screen").value = update_screen('decimal', button_value);
     }
-
     else if (event.target.classList.contains('operator')) {
         formula.push(button_value);
         document.getElementById("screen").value = update_screen('operator', button_value);
     }
-
     else if (event.target.classList.contains('clear')) {
         formula.length = 0;
         document.getElementById("screen").value = update_screen('clear', button_value);
     }
-
     else if (event.target.classList.contains('equal')) {
         solution = calculate(event);
         display_solution(solution);
@@ -57,9 +54,7 @@ function display_solution(solution) {
 }
 
 function calculate(event) {
-    // bewaar index van operator
-
-    let index_operator = 0;
+    // bewaar operator
     for (let i = 0; i < formula.length; i++) {
         if (formula[i] == '+') {
             operator = '+';
@@ -84,34 +79,30 @@ function calculate(event) {
     }
 
     // bewaar eerste en tweede nummer 
-    let first_number = 0;
-    number = []
+    number = [];
     for (let j = 0; j < index_operator; j++) {
         number.push(formula[j]);
-        x = number.join('');
-        first_number = parseFloat(x)
     }
+    x = number.join('');
+    const first_number = parseFloat(x);
 
-    let second_number = 0;
-    number_two =[];
+    number_two = [];
     for (let k = index_operator + 1; k < formula.length; k++) {
         number_two.push(formula[k]);
-        y = number_two.join('');
-        second_number = parseFloat(y)
     }
+    y = number_two.join('');
+    const second_number = parseFloat(y);
 
+    // bereken de som op basis van de operator
     if (operator == '+') {
         solution = first_number + second_number;
     }
-
     else if (operator == '-') {
         solution = first_number - second_number;
     }
-
     else if (operator == '/') {
         solution = first_number / second_number;
     }
-
     else {
         solution = first_number * second_number;
     }
